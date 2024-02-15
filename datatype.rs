3ba8e19f0ce39d0d5c95ad1ad74a7c5de9c31291
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-02-16 00:06:34 +0530

    Datatype Detailed learn

--- datatype.rs
+++ datatype.rs
@@ -4,18 +4,31 @@ fn main(){
     //types of Datatypes
 
     // 1. Scalar Datatypes
-    let number = 10; // Integer
-    let decimal = 10.0; // Float
-    let name = "DevMannan"; // String
-    let flag = true; // Boolean
-    let alpha = 'a'; // Character
+    let int_num :i8 = -128; // in this datatype where i8 is a 8 bit integer where either it will hold till positive 128 or negative 128
+    let int_num2:i16 = 32000;
+    let int_num3:i32 = 320000000;
+    let int_num4:i64 = -3200000000000000000;
+    let int_num5:i128 = -32000000000000000000000000000000000000;
 
-    println!("Number: {}, Decimal: {}, Name: {}, Flag: {}, Alpha: {}", number, decimal, name, flag, alpha);
+    let int_num6:u8 = 128; // in this datatype where u8 is a 8 bit integer where either it will hold till positive  128
+    let int_num7:u16 = 32000;
+    let int_num8:u32 = 320000000;
+    let int_num9:u64 = 3200000000000000000;
+    let int_num10:u128 =32000000000000000000000000000000000000;
 
-    if flag == true {
-        println!("The Flag you have declared is true");
-        
-    }else{
-        println!("The Flag you have declared is false");
-    }
+    
+
+    println!("-----------------------------------------Signed----------------------------------------------");
+    println!("int_num: {}", int_num);
+    println!("int_num2: {}", int_num2);
+    println!("int_num3: {}", int_num3);
+    println!("int_num4: {}", int_num4);
+    println!("int_num5: {}", int_num5);
+    //Usnsigned datatypes only positive number
+    println!("-----------------------------------------Unsigned----------------------------------------------");
+    println!("int_num: {}", int_num6);
+    println!("int_num2: {}", int_num7);
+    println!("int_num3: {}", int_num8);
+    println!("int_num4: {}", int_num9);
+    println!("int_num5: {}", int_num10);
 }
