e591c05c6015c95b920934a996d8ad25865cd114
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-02-15 20:56:05 +0530

    Variable Program

---
+++ variable.rs
@@ -0,0 +1,10 @@
+
+fn main(){
+    let x = 5;
+    let y = 10;
+    let sum = x + y;
+    let name = "DevMannan";
+    let message = "I will Win not today not tomorrow or maybe not the next month .... but i will be Champion one day";
+    println!("The Sum of {} + {} = {}", x, y, sum);
+    println!("{} says {}", name, message);
+}
