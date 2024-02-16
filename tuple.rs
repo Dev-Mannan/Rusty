35fc34e22d69a3c73a080354dccec6819c6c3563
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-02-16 22:44:17 +0530

    Tuple in Rust

---
+++ tuple.rs
@@ -0,0 +1,13 @@
+
+
+fn main(){
+
+    let tip = (500,6.42,1,"Hello");
+
+    println!("Tip is {}",tip.0);
+    println!("Tip is {}",tip.1);
+    println!("Tip is {}",tip.2);
+
+    println!("Tip is {}",tip.3);
+
+}
