03e47e09d699f755f4c54cf6ebff1176040840a3
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-02-16 22:52:41 +0530

    Mutable & Immutable Diffrence

---
+++ mut_imut.rs
@@ -0,0 +1,10 @@
+//  Mutable & Immutable in rust
+
+fn main(){
+
+    let mut age = 30;
+    println!("Age is {}",age);
+    // NOw here we can change the value as age is Mutubale 
+    age = 31;
+    println!("Age is {}",age);
+}
