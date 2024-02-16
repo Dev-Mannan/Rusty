6281166c6a8c29814d9555887f18f3cb8aa959e1
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-02-16 23:01:50 +0530

    Contant datatype in Rust

---
+++ constant.rs
@@ -0,0 +1,11 @@
+// constant in rust
+
+//  In programming if Const is mentioned that means that particular variable value cannot be changed
+
+fn main(){
+
+    const AP:i8=12; // It must be Capital letter also should be declared datatype
+
+    println!("Age is {}",AP);
+
+}
