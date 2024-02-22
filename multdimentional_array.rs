210cc1c5883877edceebafe6e393fd29c5919378
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-02-23 04:49:41 +0530

    Multidimentional array in 3D Array

---
+++ multdimentional_array.rs
@@ -0,0 +1,16 @@
+
+
+// Multi Dimentional array
+
+fn main(){
+
+    let arr1 = [[1,2,3],[4,5,6],[7,8,9]];// Multi-dimentional array
+    
+
+    println!("{:?}",arr1.len());// length of array
+
+    println!("{:?}",arr1[2][2]);// first index first value
+
+    
+
+}
