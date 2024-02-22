437907e9f2d6aa3fc70e9d39c8fee01c24ea4ca2
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-02-23 05:24:54 +0530

    Stack & Heap Memory in Rust

---
+++ Stack_heap.rs
@@ -0,0 +1,5 @@
+//  What is Stack & Heap in Rust
+
+// Stack = Memory allocation in stack , All Scalar type  can be stored in Stack Type , Size known at compile time
+// Heap = Memory allocation in heap data allocate in heap   , Size Unkonwn at compile time , Data can be change during Program Life cycle
+// less organised , slow access time ,
