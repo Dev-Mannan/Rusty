ca690d94a07221836c0589487077d5cf9dba0a8b
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-02-22 17:26:59 +0530

    For Loop Understanding in rust

---
+++ for_loop.rs
@@ -0,0 +1,15 @@
+// For Loop in Rust
+
+fn main(){
+
+    for x in 1..=10{
+        for y in 1..=10{
+
+            println!("{} x {} = {}",x,y,x*y);
+        
+            
+        }
+        println!("    ");
+    }
+
+}
